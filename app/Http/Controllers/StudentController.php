652da53c3ba42b8505f3b306b3e352b734f73d85
LeonardoFernandoSS <leonardo.fernando06@gmail.com>
2024-03-04 06:35:14 -0300

    fix: incluído metodo store e show + padrão de exceção json

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,50 +3,54 @@
 namespace App\Http\Controllers;
 
 use App\Models\Student;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-
-use function Laravel\Prompts\error;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class StudentController extends Controller
 {
-    public function __construct() {
-        
-        $this->middleware('abilities:student-index');
+    public function __construct()
+    {
+        $this->middleware('ability:student');
     }
 
     public function index()
     {
-        // $user = auth()->user;
-
-        // if (!$user->tokenCan('student-index')) {
-
-        //     return response('Unauthorized', 403);
-        // }
-
         $students = Student::all();
 
-        return response($students, 200);
+        return response()->json($students, Response::HTTP_OK);
     }
 
     public function store(Request $request)
     {
-        //
+        if (!$this->authorize('create-student')) throw new UnauthorizedHttpException('');
+
+        $student = Student::create($request->all());
+        $route = route('api.student.show', ['student' => $student->id]);
+        $headers = ["Location", $route];
+
+        return response()->json('Successfully created', Response::HTTP_CREATED, $headers);
     }
 
     public function show(string $id)
     {
-        //
+        if (!$this->authorize('detail-student')) throw new UnauthorizedHttpException('');
+
+        $student = Student::find($id);
+
+        if (is_null($student)) throw new NotFoundHttpException('');
+
+        return response()->json($student, Response::HTTP_OK);
     }
 
     public function update(Request $request, string $id)
     {
-        //
+        if (!$this->authorize('edit-student')) throw new UnauthorizedHttpException('');
     }
 
     public function destroy(string $id)
     {
-        //
+        if (!$this->authorize('delete-student')) throw new UnauthorizedHttpException('');
     }
 }
