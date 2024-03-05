a45136a49f80ea0b7af73cb28990588bbaea50f2
LeonardoFernandoSS <leonardo.fernando06@gmail.com>
2024-03-05 06:32:17 -0300

    fix: incluído atributo status em students

--- database/migrations/2024_03_01_005722_create_students_table.php
+++ database/migrations/2024_03_01_005722_create_students_table.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Enums\StatusEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -14,6 +16,7 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->enum('status', Arr::pluck(StatusEnum::cases(), 'value'))->default(StatusEnum::ACTIVE);
             $table->timestamps();
         });
     }
