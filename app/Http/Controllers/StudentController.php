6778eb2081581f775a07c2594a759650c640f343
LeonardoFernandoSS <leonardo.fernando06@gmail.com>
2024-03-05 06:33:27 -0300

    fix: ajustado StudentController para response para throw exception

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\StatusEnum;
 use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class StudentController extends Controller
 {
@@ -24,10 +24,10 @@ class StudentController extends Controller
 
     public function store(Request $request)
     {
-        if (!$this->authorize('create-student')) throw new UnauthorizedHttpException('');
+        $this->authorize('create-student', Student::class);
 
         $student = Student::create($request->all());
-        $route = route('api.student.show', ['student' => $student->id]);
+        $route = route('student.show', ['student' => $student->id]);
         $headers = ["Location", $route];
 
         return response()->json('Successfully created', Response::HTTP_CREATED, $headers);
@@ -35,22 +35,39 @@ class StudentController extends Controller
 
     public function show(string $id)
     {
-        if (!$this->authorize('detail-student')) throw new UnauthorizedHttpException('');
-
         $student = Student::find($id);
 
         if (is_null($student)) throw new NotFoundHttpException('');
+
+        $this->authorize('detail-student', $student);
 
         return response()->json($student, Response::HTTP_OK);
     }
 
     public function update(Request $request, string $id)
     {
-        if (!$this->authorize('edit-student')) throw new UnauthorizedHttpException('');
+        $student = Student::find($id);
+
+        if (is_null($student)) throw new NotFoundHttpException('');
+
+        $this->authorize('edit-student', $student);
+
+        $student->update($request->all());
+
+        return response()->json(status: Response::HTTP_NO_CONTENT);
     }
 
     public function destroy(string $id)
     {
-        if (!$this->authorize('delete-student')) throw new UnauthorizedHttpException('');
+        $student = Student::find($id);
+
+        if (is_null($student)) throw new NotFoundHttpException('');
+
+        $this->authorize('delete-student', $student);
+
+        $student->status = StatusEnum::DELETE;
+        $student->save();
+
+        return response()->json(status: Response::HTTP_NO_CONTENT);
     }
 }
