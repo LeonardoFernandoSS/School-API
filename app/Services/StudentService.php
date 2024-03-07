d2aa34ffcac61dc1869e59158ed0505b4e95d0cc
LeonardoFernandoSS <leonardo.fernando06@gmail.com>
2024-03-06 21:27:07 -0300

    feat: incluído StudentService

---
+++ app/Services/StudentService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Student;
+use App\Repositories\Interfaces\StudentRepositoryInterface;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class StudentService
+{
+    public function __construct(
+        private StudentRepositoryInterface $studentRepository
+    ) {
+    }
+
+    public function getPaginate($perPage, $page, array $search): LengthAwarePaginator
+    {
+        return $this->studentRepository->paginate($perPage, $page, $search);
+    }
+
+    public function createStudent(array $data): Student
+    {
+        return $this->studentRepository->create($data);
+    }
+
+    public function findStudent(string $id): ?Student
+    {
+        $student = $this->studentRepository->find($id);
+
+        if (is_null($student)) throw new NotFoundHttpException('');
+
+        return $student;
+    }
+
+    public function updateStudent(Student $student, array $data): Student
+    {
+        return $this->studentRepository->update($student, $data);
+    }
+
+    public function deleteStudent(Student $student): Student
+    {
+        return $this->studentRepository->delete($student);
+    }
+}
