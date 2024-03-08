f1b2a49c31017c6dc1fdd7c089adeebbd6eae459
LeonardoFernandoSS <leonardo.fernando06@gmail.com>
2024-03-07 22:41:29 -0300

    refactor: movido controllers Auth e Student para pasta Api

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\Student\SearchRequest;
 use App\Http\Requests\Student\StoreRequest;
 use App\Http\Requests\Student\UpdateRequest;
