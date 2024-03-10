61ea951e0d6f9703741c5a1ab81feb58b0248f65
LeonardoFernandoSS <leonardo.fernando06@gmail.com>
2024-03-10 14:02:16 -0300

    feat: incluído AbilityEnum

---
+++ app/Enums/AbilityEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+abstract class AbilityEnum
+{
+    const STUDENT = 'student';
+    const STUDENT_DETAIL = 'student-detail';
+    const STUDENT_MANAGE = 'student-manage';
+}
