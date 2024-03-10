f913347d279387d8e7d8d586232750ac4362039b
LeonardoFernandoSS <leonardo.fernando06@gmail.com>
2024-03-09 21:57:27 -0300

    fix: removido upload de photo em StudentService

--- app/Services/StudentService.php
+++ app/Services/StudentService.php
@@ -4,16 +4,13 @@ namespace App\Services;
 
 use App\Models\Student;
 use App\Repositories\Interfaces\StudentRepositoryInterface;
-use Illuminate\Http\UploadedFile;
 use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class StudentService
 {
-    const BASE_PHOTO_PATH = "upload/photos/student/";
-
     public function __construct(
         private StudentRepositoryInterface $studentRepository
     ) {
@@ -24,13 +21,9 @@ class StudentService
         return $this->studentRepository->paginate($perPage, $page, $search);
     }
 
-    public function createStudent(array $data, UploadedFile $photo = null): Student
+    public function createStudent(array $data): Student
     {
-        if (!is_null($photo)) {
-
-            $data['photo_path'] = $this->handlerPhoto($photo);
-        }
-
+        $data['password'] = Hash::make('password');
         $data['remember_token'] = Str::random(10);
 
         return $this->studentRepository->create($data);
@@ -45,37 +38,13 @@ class StudentService
         return $student;
     }
 
-    public function updateStudent(Student $student, array $data, UploadedFile $photo = null): Student
+    public function updateStudent(Student $student, array $data): Student
     {
-        if (!is_null($photo)) {
-
-            $data['photo_path'] = $this->handlerPhoto($photo);
-        }
-
         return $this->studentRepository->update($student, $data);
     }
 
     public function deleteStudent(Student $student): Student
     {
         return $this->studentRepository->delete($student);
-    }
-
-    public function handlerPhoto(UploadedFile $photo, ?Student $student = null): string
-    {
-        $path = self::BASE_PHOTO_PATH . time() . '.webp';
-
-        Storage::putFile($path, $photo);
-
-        if (!empty($student) && !empty($student->user)) {
-
-            $deletePath = $student->user->photo_path;
-
-            if (is_string($deletePath) && Storage::exists($deletePath)) {
-
-                Storage::delete($deletePath);
-            }
-        }
-
-        return $path;
     }
 }
