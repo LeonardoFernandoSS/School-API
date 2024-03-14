62ba1f11126946c18fce8a7058089b368d1c6894
LeonardoFernandoSS <leonardo.fernando06@gmail.com>
2024-03-13 21:50:58 -0300

    fix: incluída permissões ability, role e user

--- app/Enums/AbilityEnum.php
+++ app/Enums/AbilityEnum.php
@@ -4,6 +4,12 @@ namespace App\Enums;
 
 abstract class AbilityEnum
 {
+    const ABILITY = 'ability';
+    const ROLE = 'role';
+    const ROLE_MANAGE = 'role-manage';
+    const USER = 'user';
+    const USER_DETAIL = 'user-detail';
+    const USER_MANAGE = 'user-manage';
     const STUDENT = 'student';
     const STUDENT_DETAIL = 'student-detail';
     const STUDENT_MANAGE = 'student-manage';
