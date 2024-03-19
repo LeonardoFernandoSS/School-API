2c31c6a1269a389512ab2476e2a7cc28a513e825
LeonardoFernandoSS <leonardo.fernando06@gmail.com>
2024-03-19 06:32:33 -0300

    feat: incluído user events

---
+++ app/Events/User/DeleteUser.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Events\User;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class DeleteUser implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public User $user)
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('user'),
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'DeleteUser';
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'user' => new UserResource($this->user)
+        ];
+    }
+}
