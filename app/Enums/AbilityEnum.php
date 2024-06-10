925fc49ae82e0bf9c538af66af413da2ea895be5
LeonardoFernandoSS <leonardo.fernando06@gmail.com>
2024-06-10 06:28:07 -0300

    fix: incluído abilities de acordo com as migrations

--- app/Enums/AbilityEnum.php
+++ app/Enums/AbilityEnum.php
@@ -13,6 +13,27 @@ abstract class AbilityEnum
     const STUDENT = 'student';
     const STUDENT_DETAIL = 'student-detail';
     const STUDENT_MANAGE = 'student-manage';
+    const TEACHER = 'teacher';
+    const TEACHER_DETAIL = 'teacher-detail';
+    const TEACHER_MANAGE = 'teacher-manage';
+    const PERIOD = 'period';
+    const PERIOD_DETAIL = 'period-detail';
+    const PERIOD_MANAGE = 'period-manage';
+    const COURSE = 'course';
+    const COURSE_DETAIL = 'course-detail';
+    const COURSE_MANAGE = 'course-manage';
+    const ENROLLMENT = 'enrollment';
+    const ENROLLMENT_DETAIL = 'enrollment-detail';
+    const ENROLLMENT_MANAGE = 'enrollment-manage';
+    const DISCIPLINE = 'discipline';
+    const DISCIPLINE_DETAIL = 'discipline-detail';
+    const DISCIPLINE_MANAGE = 'discipline-manage';
+    const CURRICULUM = 'curriculum';
+    const CURRICULUM_DETAIL = 'curriculum-detail';
+    const CURRICULUM_MANAGE = 'curriculum-manage';
+    const CLASSROOM = 'classroom';
+    const CLASSROOM_DETAIL = 'classroom-detail';
+    const CLASSROOM_MANAGE = 'classroom-manage';
 
     public static function toArray()
     {
