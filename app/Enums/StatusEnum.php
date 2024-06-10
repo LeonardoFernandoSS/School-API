27c8a62760f6574ea6033abc14a70072e01f5d4f
LeonardoFernandoSS <leonardo.fernando06@gmail.com>
2024-06-10 06:29:13 -0300

    fix: incluído metodo toArray em StatusEnum

--- app/Enums/StatusEnum.php
+++ app/Enums/StatusEnum.php
@@ -7,4 +7,9 @@ enum StatusEnum: string
     case ACTIVE = 'active';
     case BLOCK = 'block';
     case DELETE = 'delete';
+
+    public static function toArray(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
 }
