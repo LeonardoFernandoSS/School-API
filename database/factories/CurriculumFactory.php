7f630748b19ece4056de25a4e9a5da3e24e1408a
LeonardoFernandoSS <leonardo.fernando06@gmail.com>
2024-06-11 06:38:29 -0300

    fix: incluído entidade Curriculum e dependentes

---
+++ database/factories/CurriculumFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\StatusEnum;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class CurriculumFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'status' => StatusEnum::ACTIVE,
+        ];
+    }
+}
