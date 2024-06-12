685c9c6ef40888d268c0021a62f4a39c8dab3904
LeonardoFernandoSS <leonardo.fernando06@gmail.com>
2024-06-12 06:19:32 -0300

    fix: incluído relacionamento enrollments em Student

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -7,6 +7,7 @@ use App\Models\Scopes\UserScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Student extends Model
 {
@@ -14,7 +15,6 @@ class Student extends Model
 
     protected $fillable = [
         'name',
-        'status',
     ];
 
     protected $guarded = [
@@ -33,5 +33,10 @@ class Student extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class)->withoutGlobalScope(UserScope::class);
+    }
+
+    public function enrollments(): HasMany
+    {
+        return $this->hasMany(Enrollment::class);
     }
 }
